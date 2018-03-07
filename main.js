03f009b9823a8a9716b318e37aac70c85f4072a6
Bernardo Vieira <bernardo5@live.com.pt>
2018-03-07 23:55:47 +0000

    adjusts

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
 var inquirer = require('inquirer');
 var exec = require('exec-sh');
 
-var objservers = {'tst_auto':['(10.10) fe', '(12.10) be'], 'dev':['(20.10) fe', '(22.10) be']};
+var objservers = {
+	'tst_auto':
+	[
+		'(10.10) fe', 
+		'(12.10) be'
+	],
+	'dev':
+	[
+		'(20.10) fe', 
+		'(22.10) be'
+	]
+};
 
 
-var servers = []
-for(var exKey in objservers) {
-    servers.push(exKey);
-}
 
+inquirer.prompt({
+	type: 'list',
+	name: 'environment',
+	message: "Which environment?",
+	choices: () => {
+		var servers = []
+		for(var exKey in objservers) {
+			servers.push(exKey);
+		}
+		return servers
+	}
+}).then(answers => {
 
-inquirer
-  .prompt({
-    type: 'list',
-    name: 'environment',
-    message: "Which environment?",
-    choices: servers
-  })
-  .then(answers => {
-	console.log(answers.environment)
-	
-	var question1 = {
+	var questionServer = {
 		type: 'list',
 		name: 'server',
 		message: "Which server?",
 		choices: objservers[answers.environment]
 	}
 
-    inquirer.prompt([question1]).then(answers => {
-		console.log(answers.server)
+	inquirer.prompt([questionServer]).then(answers => {
 
 		var str = answers.server;
 		var regexp = /\((.*)\)/;
 		var matches_array = str.match(regexp)[1]
 
-		console.log(matches_array)
-
-		exec("echo lorem && bash", { cwd: "/home" }, function(err){
+		exec("ssh user@" + matches_array, function(err) {
 			if (err) {
-			  console.log("Exit code: ", err.code);
-			  return;
+				console.log("Exit code: ", err.code);
+				return;
 			}
-		  });
+		});
 	});
 });
