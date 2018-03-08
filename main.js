90bf2a86b7f97d54dd8c138908bd997daccdb1cd
Bernardo Vieira <bernardo5@live.com.pt>
2018-03-08 00:37:16 +0000

    added datasaver

--- main.js
+++ main.js
@@ -1,55 +1,24 @@
 var inquirer = require('inquirer');
 var exec = require('exec-sh');
-
-var appData = {
-	'tst_auto':
-	[
-		{
-			'name': 'fe',
-			'ip': '10.10',
-			'user': 'myuuser',
-			'password': 'pass'
-		}, 
-		{
-			'name': 'be',
-			'ip': '10.20',
-			'user': 'myuuser',
-			'password': 'pass'
-		}
-	],
-	'dev':
-	[
-		{
-			'name': 'fe',
-			'ip': '20.10',
-			'user': 'myuuser',
-			'password': 'pass'
-		},
-		{
-			'name': 'be',
-			'ip': '20.20',
-			'user': 'myuuser',
-			'password': 'pass'
-		}
-	]
-}
+var dataSaver = require('./modules/datasaver');
 
 
+var appData = JSON.parse(dataSaver.load());
 
 inquirer.prompt({
 	type: 'list',
 	name: 'environment',
 	message: 'Which environment?',
 	choices: () => {
-		var groups = []
+		var groups = [];
 		for(var group in appData) {
-			groups.push(group)
+			groups.push(group);
 		}
-		return groups
+		return groups;
 	}
 }).then(answers => {
 
-	var actualEnvironment = answers.environment
+	var actualEnvironment = answers.environment;
 	var questionServer = {
 		type: 'list',
 		name: 'server',
@@ -57,27 +26,27 @@ inquirer.prompt({
 		choices: () => {
 			var servers = []
 			appData[answers.environment].forEach(arrayItem => {
-				servers.push(arrayItem.name)
+				servers.push(arrayItem.name);
 			})
-			return servers
+			return servers;
 		}
-	}
+	};
 
 	inquirer.prompt([questionServer]).then(answers => {
 
-		var serverInfo
+		var serverInfo;
 		appData[actualEnvironment].forEach(arrayItem => {
 			if(arrayItem.name == answers.server) {
-				serverInfo = arrayItem
-				return
+				serverInfo = arrayItem;
+				return;
 			}
-		})
+		});
 
 		exec('ssh ' + serverInfo.user + '@' + serverInfo.ip, err => {
 			if (err) {
-				console.log('Exit code: ', err.code)
-				return
+				console.log('Exit code: ', err.code);
+				return;
 			}
-		})
-	})
-})
+		});
+	});
+});
--- modules/datasaver.js
+++ modules/datasaver.js
@@ -5,23 +5,12 @@ const fileName = '/.lazaro/data.boy'
 module.exports = {
 
     load : function() {
-        //return '[{"name" : "Test", "type" : "group", "elements" : [{"name" : "John Doe"}]}, {"name" : "You", "type" : "element"}]'
         if(fs.existsSync(os.homedir() + fileName)) {
             return fs.readFileSync(os.homedir() + fileName, 'utf8');
         }
         return '[]'
     },
-
-    getOne : function(ip) {
-        var allResults = load()
-        /* apply some filter */
-        return '[]'
-    },
-
-    addOnde: function(data) {
-
-    },
-
+    
     save : function(dataToSave) {
         return fs.writeFileSync(os.homedir() + fileName, dataToSave, 'utf8')
     }
